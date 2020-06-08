88c020dedfab5efcf782606c886a8deb45360ccc
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-08 17:49:05 +0100

    Location handler and Uber Request

--- Simple Uber Clone/RiderViewController.swift
+++ Simple Uber Clone/RiderViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import MapKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class RiderViewController: UIViewController, CLLocationManagerDelegate {
 
@@ -15,6 +17,8 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var callAnUberButton: UIButton!
     
     var locationManager = CLLocationManager()
+    var userlocation = CLLocationCoordinate2D()
+    var uberHasBeenCalled = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +34,7 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let coord = manager.location?.coordinate{
             let center = CLLocationCoordinate2D(latitude: coord.latitude, longitude: coord.longitude)
+            userlocation = center
             let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
             map.setRegion(region, animated: true)
             
@@ -42,6 +47,25 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func logoutTapped(_ sender: Any) {
+        if let email = Auth.auth().currentUser?.email{
+            if uberHasBeenCalled{
+                uberHasBeenCalled = false
+                callAnUberButton.setTitle("Call an Uber", for: .normal)
+                Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded) { (snapshot) in
+                    snapshot.ref.removeValue()
+                    Database.database().reference().child("RideRequests").removeAllObservers()
+                }
+            }
+            else{
+                let rideRequestDictionary : [String:Any] = ["email": email,"lat":userlocation.latitude, "lon":userlocation.longitude]
+                Database.database().reference().child("RideRequests").childByAutoId().setValue(rideRequestDictionary)
+                uberHasBeenCalled = true
+                callAnUberButton.setTitle("Cancel Uber", for: .normal)
+            }
+            
+            
+        }
+        
     }
     @IBAction func callUberTapped(_ sender: Any) {
     }
